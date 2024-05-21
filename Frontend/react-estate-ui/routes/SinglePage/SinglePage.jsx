6ad2ad55fdf6a16c3c2c2a8eda80f3c0278156d9
hrithikpandey786 <hrithikpandey786@gmail.com>
2024-05-22 04:49:02 +0530

    single page done

--- Frontend/react-estate-ui/routes/SinglePage/SinglePage.jsx
+++ Frontend/react-estate-ui/routes/SinglePage/SinglePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "../../components/Slider/Slider";
 import "./singlePage.scss";
 import { singlePostData, userData } from "../../lib/dummydata";
+import Map from "../../components/Map/Map"
 
 export default function SinglePage(){
     return (
@@ -32,7 +33,83 @@ export default function SinglePage(){
             </div>
             <div className="features">
                 <div className="wrapper">
-                    Wrapper
+                    <p className="title">General</p>
+                    <div className="listVertical">
+                        <div className="feature">
+                            <img src="utility.png"></img>
+                            <div className="featureText">
+                                <span>Utilities</span>
+                                <p>Renter is responsible</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="pet.png"></img>
+                            <div className="featureText">
+                                <span>Pet Policy</span>
+                                <p>Pet Allowed</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="fee.png"></img>
+                            <div className="featureText">
+                                <span>Property Fees</span>
+                                <p>Must have 3x the rent in total householed income</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="title">Room Sizes</p>
+                    <div className="sizes">
+                        <div className="size">
+                            <img src="/size.png"></img>
+                            <span>50sqrm 861(sqft)</span>
+                        </div>
+                        <div className="size">
+                            <img src="/bed.png"></img>
+                            <span>2 bed</span>
+                        </div>
+                        <div className="size">
+                            <img src="/bath.png"></img>
+                            <span>1 bathroom</span>
+                        </div>
+                    </div>
+                    <p className="title">Nearby Places</p>
+                    <div className="listHorizontal">
+                        <div className="feature">
+                                <img src="/school.png"></img>
+                                <div className="featureText">
+                                    <span>School</span>
+                                    <p>250m away</p>
+                                </div>
+                            </div>
+                            <div className="feature">
+                                <img src="/bus.png"></img>
+                                <div className="featureText">
+                                    <span>Bus Stop</span>
+                                    <p>100m away</p>
+                                </div>
+                            </div>
+                            <div className="feature">
+                                <img src="/restaurant.png"></img>
+                                <div className="featureText">
+                                    <span>Restaurant</span>
+                                    <p>200m away</p>
+                                </div>
+                            </div>
+                    </div>
+                    <p className="title">Location</p>
+                    <div className="mapContainer">
+                        <Map items={[singlePostData]}/>
+                   </div>
+                    <div className="buttons">
+                        <div className="button">
+                            <img src="/chat.png"></img>
+                            <p>Send a Message</p>
+                        </div>
+                        <div className="button">
+                            <img src="save.png"></img>
+                            <p>Save the Place</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
